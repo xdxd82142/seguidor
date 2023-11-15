2f739594dc289f43a61f3b4daf48dab165949ef8
xdxd82142 <150831643+xdxd82142@users.noreply.github.com>
2023-11-15 01:54:16 -0600

    Update seguidor.ino

--- seguidor.ino
+++ seguidor.ino
@@ -9,24 +9,30 @@ int INB = 9;
 int infrarojo_izquierdo = 10;
 int infrarojo_derecho = 11;
 int infrarojo_medio = 6;
+int infrarojo_lateral_derecho = 12;
+int infrarojo_lateral_izquierdo = 4;
 
 // Variables de entorno
-int velocidad = 150;
-int tiempo_ejecucion = 150;
-int negro = 0;
-int blanco = 1;
+int velocidad = 95;
+int tiempo_ejecucion = 5;
+int retraso = 1;
+int negro = 1;
+int blanco = 0;
 
 // Variables para los sensores
 int valor_inicial_infrarojo_izq = 0;
 int valor_inicial_infrarojo_der = 0;
 int valor_inicial_infrarojo_med = 0;
+int valor_inicial_infrarojo_lat_der = 0;
+int valor_inicial_infrarojo_lat_izq = 0;
 
 void setup(){
   Serial.begin(9600);
-
   pinMode(infrarojo_izquierdo, INPUT);
   pinMode(infrarojo_derecho, INPUT);
   pinMode(infrarojo_medio, INPUT);
+  pinMode(infrarojo_lateral_derecho, INPUT);
+  pinMode(infrarojo_lateral_izquierdo, INPUT);
 
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -43,100 +49,102 @@ void setup(){
 
 void loop(){
 
-  valor_inicial_infrarojo_izq = digitalRead(infrarojo_izquierdo);
-  valor_inicial_infrarojo_der = digitalRead(infrarojo_derecho);
-  valor_inicial_infrarojo_med = digitalRead(infrarojo_medio);
+  while (true){
+    valor_inicial_infrarojo_izq = digitalRead(infrarojo_izquierdo);
+    valor_inicial_infrarojo_der = digitalRead(infrarojo_derecho);
+    valor_inicial_infrarojo_med = digitalRead(infrarojo_medio);
+    valor_inicial_infrarojo_lat_der = digitalRead(infrarojo_lateral_derecho);
+    valor_inicial_infrarojo_lat_izq = digitalRead(infrarojo_lateral_izquierdo);
 
-  Serial.println("Variables:::::: ");
-  Serial.println("El valor izquierdo "); Serial.println(valor_inicial_infrarojo_izq);
-  Serial.println("El valor derecho "); Serial.println(valor_inicial_infrarojo_der);
-  Serial.println("El valor medio "); Serial.println(valor_inicial_infrarojo_med);
- 
-  if (valor_inicial_infrarojo_izq == blanco && valor_inicial_infrarojo_med == negro && valor_inicial_infrarojo_der == blanco){
-    movimiento_delante();
-    Serial.println("primer if");
+    
+    if (valor_inicial_infrarojo_izq == blanco && valor_inicial_infrarojo_med == negro && valor_inicial_infrarojo_der == blanco){
+      movimiento_delante();
+    }
+    else if (valor_inicial_infrarojo_izq == negro && valor_inicial_infrarojo_med == blanco && valor_inicial_infrarojo_der == negro){
+      movimiento_delante();
+    }
+    else if (valor_inicial_infrarojo_izq == negro && valor_inicial_infrarojo_med == negro && valor_inicial_infrarojo_der == negro){
+      movimiento_delante();
+    }
+    // Girar izquierda
+    else if ( valor_inicial_infrarojo_izq == blanco || valor_inicial_infrarojo_lat_izq == blanco && valor_inicial_infrarojo_med == negro && valor_inicial_infrarojo_der == negro){
+      movimiento_derecha();
+    }
+    else if ( valor_inicial_infrarojo_izq == blanco || valor_inicial_infrarojo_lat_izq == blanco && valor_inicial_infrarojo_med == blanco && valor_inicial_infrarojo_der == negro){
+      movimiento_derecha();
+    }
+    // Girar derecha
+    else if (valor_inicial_infrarojo_izq == negro && valor_inicial_infrarojo_med == blanco && valor_inicial_infrarojo_der == blanco || valor_inicial_infrarojo_lat_der == blanco){
+      movimiento_izquierda();
+    }
+    else if (valor_inicial_infrarojo_izq == negro && valor_inicial_infrarojo_med == negro && valor_inicial_infrarojo_der == blanco || valor_inicial_infrarojo_lat_der == blanco){
+      movimiento_izquierda();
+    }
+    else if (valor_inicial_infrarojo_izq == blanco && valor_inicial_infrarojo_med == blanco && valor_inicial_infrarojo_der == blanco){
+      movimiento_delante();
+    }
 
+
+    if (valor_inicial_infrarojo_lat_izq == negro && valor_inicial_infrarojo_izq != negro){
+      movimiento_derecha();
+    }
+    if (valor_inicial_infrarojo_lat_der == negro && valor_inicial_infrarojo_der != negro){
+      movimiento_izquierda();
+    }
   }
-
-  else if (valor_inicial_infrarojo_izq == negro && valor_inicial_infrarojo_med == negro && valor_inicial_infrarojo_der == negro){
-    movimiento_delante();
-    Serial.println("segundo if delante");
-  }
-  
-  // Girar izquierda
-  else if (valor_inicial_infrarojo_izq == blanco && valor_inicial_infrarojo_med == blanco && valor_inicial_infrarojo_der == negro){
-    movimiento_izquierda();
-    Serial.println("primer elif izquierda");
- 
-  }
-
-  else if (valor_inicial_infrarojo_izq == blanco && valor_inicial_infrarojo_med == negro && valor_inicial_infrarojo_der == negro){
-    movimiento_izquierda();
-    Serial.println("segundo elif izquierda");
-  }
-
-  // Girar derecha
-  else if (valor_inicial_infrarojo_izq == negro && valor_inicial_infrarojo_med == blanco && valor_inicial_infrarojo_der == blanco){
-    movimiento_derecha();
-    Serial.println("primer elif derecha");
-  }
-
-  else if (valor_inicial_infrarojo_izq == negro && valor_inicial_infrarojo_med == negro && valor_inicial_infrarojo_der == blanco){
-    movimiento_derecha();
-    Serial.println("primer elif derecha");
-  }
-
-  else if (valor_inicial_infrarojo_izq == blanco && valor_inicial_infrarojo_med == blanco && valor_inicial_infrarojo_der == blanco){
-    movimiento_derecha();
-    Serial.println("elif para el caso en que este fuera de la linea ");
-  }
-
-  else{
-    movimiento_delante();
-    Serial.println("Else ");
-  }
-  
-
-  detenerse();
-  
 
 }
 
-void movimiento_delante(){
-  
-  digitalWrite(INB, velocidad);
+void movimiento_delante(){ 
+  analogWrite(INB, velocidad);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
 
-
-  digitalWrite(INA, velocidad);
+  analogWrite(INA, velocidad);
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   delay(tiempo_ejecucion);
+}
 
+void movimiento_atras(){
+  analogWrite(INB, velocidad);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  analogWrite(INA, velocidad);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  delay(tiempo_ejecucion);
 }
 
 void movimiento_derecha(){
-  digitalWrite(INB, velocidad);
+
+  analogWrite(INB, velocidad+75);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  delay(tiempo_ejecucion-100);
 
+  analogWrite(INA, velocidad);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  delay(tiempo_ejecucion);
 }
 
 void movimiento_izquierda(){
 
-  digitalWrite(INA, velocidad);
+  analogWrite(INA, velocidad+75);
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
-  delay(tiempo_ejecucion-100);
 
+  analogWrite(INB, velocidad);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  delay(tiempo_ejecucion);
 }
 
 void detenerse(){
-  digitalWrite(INA, LOW);
-  digitalWrite(INB, LOW);
-  delay(100);
+  analogWrite(INA, 0);
+  analogWrite(INB, 0);
+  delay(retraso);
 }
 
 
